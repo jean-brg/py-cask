3c667072b7ee713ea4e1191a9e2779f5fdf8c55a
Jean Bourguignon <133725375+jean-brg@users.noreply.github.com>
2026-06-28 21:09:08 -0400

    Delete example_app directory, keeping only python library

--- example_app/static/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const button = document.getElementById('alertBtn');
-    if (button) {
-        button.addEventListener('click', () => {
-            alert("Some JavaScript code!");
-        });
-    }
-});
